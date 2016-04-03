263047d9c441822d677e60f77686e18c0ae2c1cc
sei iwanaga <kayurra@gmail.com>
2016-04-03 19:26:18 +0900

    routing事始め

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Node World");
+    response.write("Hello World");
     response.end();
   }
 
