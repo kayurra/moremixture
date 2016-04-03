2d6148c001d9bce69abc73ae306000c4d5bc8cd5
sei iwanaga <kayurra@gmail.com>
2016-04-03 20:35:10 +0900

    ブロッキング完了

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,13 +4,11 @@ function start(response) {
   console.log("Request handler 'start' was called.");
 
   // ブロッキング確認用のコード
-  exec("find /",
-    { timeout: 10000, maxBuffer: 20000*1024 },
-    function (error, stdout, stderr) {
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write(stdout);
-      response.end();
-    });
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
 function commit(response) {
