2f33c25538b90d261053e5b97c088420dd6d07f3
sei iwanaga <kayurra@gmail.com>
2016-04-03 21:06:09 +0900

    post it!

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,34 @@
 var exec = require("child_process").exec;
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   // ブロッキング確認用のコード
   exec("ls -lah", function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
-    response.end();
+      var body = '<html>'+
+          '<head>'+
+          '<meta http-equiv="Content-Type" content="text/html; '+
+          'charset=UTF-8" />'+
+          '</head>'+
+          '<body>'+
+          '<form action="/commit" method="post">'+
+          '<textarea name="text" rows="20" cols="60"></textarea>'+
+          '<input type="submit" value="Submit text" />'+
+          '</form>'+
+          '</body>'+
+          '</html>';
+
+          response.writeHead(200, {"Content-Type": "text/html"});
+          response.write(body);
+          response.end();
   });
 }
 
-function commit(response) {
+function commit(response, postData) {
   console.log("Request handler 'commit' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Commit");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hello Commit </br>");
+  response.write("You've post: " + postData);
   response.end();
 }
 
