556d84e3377f91ac3f94a5e020d4c70deeb1400f
sei iwanaga <kayurra@gmail.com>
2016-04-03 20:05:15 +0900

    non broking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,21 @@
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
+
+  // ブロッキング確認用のコード
+  exec("find /",
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function (error, stdout, stderr) {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write(stdout);
+      response.end();
+    });
 }
 
-function commit() {
+function commit(response) {
   console.log("Request handler 'commit' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Commit");
+  response.end();
 }
 
 exports.start = start;
